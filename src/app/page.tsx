4fa2f495a3983847c968299b167c0c42ec3092ca
Zainab0612 <znaeem0612@gmail.com>
2024-07-18 00:52:28 -0700

    Changes Done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,18 @@
 import { Typography, Box } from "@mui/joy";
 import CountdownTimer from "@/component/timer/timer";
 import Button from "@/component/button/button";
+import Logo from "@/component/logo/logo";
 
 export default function Home() {
 
   const targetDate = new Date('08/12/2024 23:59:59');
   return (
     <Box sx={{
-      backgroundImage: 'url(/assets/images/Background.png)',
-      backgroundRepeat: 'no-repeat',
-      backgroundPosition: 'center',
-      backgroundSize: "cover",
+      overflow: 'hidden'
     }}>
-      <main className="flex min-h-screen flex-col items-center p-24">
+      <main className="flex min-h-screen flex-col items-center justify-center p-20">
         <div className="logo">
+          <Logo />
         </div>
         <div>
           <Typography sx={{
@@ -28,7 +27,14 @@ export default function Home() {
             fontWeight: "200",
             textShadow: "0 3px 10px rgba(0, 0, 0, 0.78)",
             color: "white",
-            margin: '66px -23px 20px -22px',
+            margin: {
+              xs: '66px -23px 20px -22px',
+              sm: '66px -23px 20px -22px',
+              md: '23px -23px 20px -22px',
+              lg: '23px -23px 20px -22px',
+              xl: '66px -23px 20px -22px',
+              
+            },
             lineHeight: "2.5",
           }} className="text">Something great is on the way</Typography>
         </div>
--- src/component/logo/logo.tsx
+++ src/component/logo/logo.tsx
@@ -1,10 +1,27 @@
-import { Box } from "@mui/joy"
-import Image from "next/image"
+import { Box } from "@mui/joy";
+import Image from 'next/image';
+import logoImg from '../../assets/images/Group 120.png';
+
 const logo = () => {
     return (
         <>
-            <Box>
-               <img src=""></img>
+            <Box sx={{
+                width: {
+                    xs: 300,
+                    sm: 350,
+                    md: 400,
+                    lg: 450,
+                    xl: 500
+                },
+                height: {
+                    xs: 100,
+                    sm: 120,
+                    md: 140,
+                    lg: 160,
+                    xl: 170
+                },
+            }}>
+                <Image src={logoImg} alt="logo" />
             </Box>
         </>
     )
