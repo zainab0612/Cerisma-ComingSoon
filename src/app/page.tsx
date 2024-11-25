d2b544b956b38f62325eeb7c4bfbebc26d0d1dfe
Zainab0612 <znaeem0612@gmail.com>
2024-11-25 18:29:27 +0500

    timer change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Logo from "@/component/logo/logo";
 
 const Home = () => {
 
-  const targetDate = new Date('11/01/2024 22:59:59');
+  const targetDate = new Date('02/01/2025 22:59:59');
 
   return (
     <Box sx={{
